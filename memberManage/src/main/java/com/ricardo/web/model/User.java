8a6840d066acae467fd6cec2975af353d6441dfe
jli033 <jli033@2008.sina.com>
2016-08-25 18:57:39 +0900

    mae

---
+++ memberManage/src/main/java/com/ricardo/web/model/User.java
@@ -0,0 +1,19 @@
+package com.ricardo.web.model;
+public class User {
+	private String loginID;
+	private String password;
+	//-------------------------------
+	public String getLoginID() {
+		return loginID;
+	}
+	public void setLoginID(String loginID) {
+		this.loginID = loginID;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
